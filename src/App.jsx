61d67cd4f321e1e0d7783140a7ceffa9882b0520
Mario Ubeira <mario@pumpunhost.com>
2025-04-09 10:34:38 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>¡Hola React! 🎉</h1>
+      <p>Mi primera app hecha con Vite y React.</p>
+    </div>
+  );
+}
+
+export default App
